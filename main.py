5d9a780b7448a747b73ed74ef7885532aeffd090
Chemistry Freak <nawi.chemie.freak@gmail.com>
2020-04-06 21:46:59 +0200

    overworked Room & Player class and xml system

--- main.py
+++ main.py
@@ -2,22 +2,39 @@ from lxml import etree, objectify
 
 with open('game1.xml') as f:
     test = f.read()
-world = objectify.fromstring(test)
+world = objectify.fromstring(test)           
+
+rooms = []
+
+def initialize():
+    global rooms
+    for child in world.getchildren():
+        if child.tag == "room":
+            rooms.append(Room(child))
+            print("Initialized Room " + child.get("id"))
 
 class Room:
     def __init__(self, room):
         self.connections = [] 
         self.isactive = False
-        if hasattr(room, 'player'): # 
+        if hasattr(room, 'player'):
             self.isactive = True
             global player
-            player = Player(room.player, room.get('id'))
+            player = Player(room.player, room.get("id"))
         self.connections = str(room.connectsto).split(',')
+        return
 
 class Player:
     def __init__(self, player, inroom):
+        self.name = input("What's your name?\n")
         self.inroom = inroom
-        self.name = input('What\'s your name?\n')
-        
-activeroom = Room(world.room[0])
-print(player.name)
+        return
+    def update(self, player, inroom):
+        self.inroom = inroom
+        return
+
+initialize()
+print(rooms)
+print(rooms[0].isactive, rooms[0].connections)
+print(player.name, player.inroom)
+
