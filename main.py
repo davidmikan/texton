9acd0f1f256839ad2ed510c4c3502345e5ab3d61
David Mikan <itsmikanakg@gmail.com>
2020-05-22 12:54:51 +0200

    main.py

--- main.py
+++ main.py
@@ -17,25 +17,30 @@ def go_to_room(uinput):
     else:
         world.say('room_notfound')
 
+def save(uinput):
+    world.say('world_saving')
+    world.save(uinput)
+    world.say('world_saved')
+
 def exit(uinput):
-    world.save('files/test.xml')
-    print(uinput)
+    save(uinput)
+    world.say('world_quit')
     quit()
 
-world = wrld.World('files/game1.xml')
-commands = {
-    'exit': exit,
-    'go to': go_to_room
-}
-print('SESSION: ' + str(world.get_property('session')))
-world.say('world_loaded')
+if __name__ == '__main__':
+    world = wrld.World('files/game1.xml')
+    commands = {
+        'exit': exit,
+        'save': save,
+        'go to': go_to_room
+    }
+    print('SESSION: ' + str(world.get_property('session')))
 
-while True:
-    print('\nSTEP: ' + str(world.get_property('steps')))
-    uinput = input('> ')
-    for command in commands:
-        if uinput.startswith(command):
-            world.step()
-            commands[command](uinput[len(command):].strip())
-            break
-    
+    while True:
+        print('\nSTEP: ' + str(world.get_property('steps')))
+        uinput = input('> ')
+        for command in commands:
+            if uinput.startswith(command):
+                world.step()
+                commands[command](uinput[len(command):].strip())
+                break
